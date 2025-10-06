3ebd753ec8f089571ec13ab9340c67acc8576dfb
yihan.lee <natsuejji1@gmail.com>
2025-10-07 01:39:19 +0800

    refactor(policy): Clean up and standardize QLinearHead module

--- Policy/QLinear.py
+++ Policy/QLinear.py
@@ -1,6 +1,8 @@
+import torch.nn as nn
+
 class QLinearHead(nn.Module):
-    def __init__(self, in_dim: int, out_actions: int, bias=True, init=None, bias_init=None):
+    def __init__(self, in_features: int, out_actions: int, bias: bool = True):
         super().__init__()
-        self.linear = nn.Linear(in_dim, out_actions, bias=bias)
-        _init(self.linear, init, bias_init)
-    def forward(self, feat): return self.linear(feat)
+        self.linear = nn.Linear(in_features, out_actions, bias=bias)
+    def forward(self, x):
+        return self.linear(x)  # [B, num_actions]
